fe79c89aa648907104fa52582d03e90a8a6de888
Oren Peer <oren.peer2@gmail.com>
2020-03-28 18:57:39 +0300

    again

--- code/SiameseNetwork.py
+++ code/SiameseNetwork.py
@@ -22,10 +22,10 @@ class SiameseNetwork(nn.Module):
         root_folder = Path(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
         resnet50_model = resnet50_ft(
             root_folder / 'pre_trained_models' / 'resnet50_ft_pytorch' / 'resnet50_ft_dims_2048.pth')
-        resnet50_128_model = resnet50_128(
-            root_folder / 'pre_trained_models' / 'resnet50_128_pytorch' / 'resnet50_128.pth')
-        senet50_256_model = senet50_256(
-            root_folder / 'pre_trained_models' / 'senet50_256_pytorch' / 'senet50_256.pth')
+        # resnet50_128_model = resnet50_128(
+        #     root_folder / 'pre_trained_models' / 'resnet50_128_pytorch' / 'resnet50_128.pth')
+        # senet50_256_model = senet50_256(
+        #     root_folder / 'pre_trained_models' / 'senet50_256_pytorch' / 'senet50_256.pth')
 
         pretrained_model = resnet50_model
 
@@ -39,42 +39,24 @@ class SiameseNetwork(nn.Module):
         # common part ('siamese')
         # self.model.classifier = self.model.classifier[:-1]
         # Separate part - 2 featurs_vectors -> one long vector -> classify:
-        self.classifier = nn.Sequential(nn.Linear(2 * num_features, 100),
+        self.classifier = nn.Sequential(nn.Linear(4 * num_features, 100),
                                         nn.ReLU(),
-                                        # nn.Dropout(0.3),
+                                        nn.Dropout(0.3),
                                         nn.Linear(100, 25),
                                         nn.ReLU(),
-                                        # nn.Dropout(0.3),
+                                        nn.Dropout(0.3),
                                         nn.Linear(25, 1),
                                         nn.Sigmoid()
                                         )
-
-        # features_layers = self.features.modules()
-        # num_layers = len(features_layers)
-        # num_trainable_layers = 3
-
-        # for x in features_layers[:-num_trainable_layers]:
-        #     x.requires_grad = False
-        # for x in features_layers[-num_trainable_layers:]:
-        #     x.requires_grad = True
-        #     print(x, ": Not frozen!")
-
-        # for i, (name, param) in enumerate(features_layers):
-        #     print(name)
-        #     if i < num_layers - num_trainable_layers:
-        #         param.requires_grad = False
-        #     else:
-        #         param.requires_grad = True
-        #         print(i, name, ": Not frozen!")
-
-        for param in self.features.parameters():
-            param.requires_grad = True
-        for param in self.features.classifier.parameters():
-            param.requires_grad = False
-
-
-        # for i, param in enumerate(self.model.classifier.parameters()):
+        # for param in self.features.parameters():
         #     param.requires_grad = False
+
+        for i, (name, param) in enumerate(self.features.named_parameters()):
+            if i < 158:
+                param.requires_grad = False
+            else:
+                param.requires_grad = True
+                print(i, name, ": Not frozen!")
 
         self.initialize()
 
@@ -82,21 +64,21 @@ class SiameseNetwork(nn.Module):
         # self.model.classifier.add_module('Our Sigmoid', nn.Sigmoid())
 
     def forward(self, input1, input2):
-        feat1 = self.features(input1)
-        f1 = feat1.view(feat1.size()[0], -1)  # make it suitable for fc layer.
+        feat1 = self.features(input1)[1]
+        # feat1 = feat1.view(feat1.size()[0], -1)  # make it suitable for fc layer.
         # feat1 /= torch.sqrt(torch.sum(feat1**2, dim=1, keepdim=True))
-        feat2 = self.features(input2)
-        f2 = feat2.view(feat2.size()[0], -1)  # make it suitable for fc layer.
+        feat2 = self.features(input2)[1]
+        # feat2 = feat2.view(feat2.size()[0], -1)  # make it suitable for fc layer.
         # feat2 /= torch.sqrt(torch.sum(feat2 ** 2, dim=1, keepdim=True))
         # feat = feat1 + feat2
 
-        # f1_max = torch.nn.functional.max_pool2d(feat1, kernel_size=feat1.size()[2:])
-        # f1_avg = nn.functional.avg_pool2d(feat1, kernel_size=feat1.size()[2:])
-        # f1 = torch.cat((f1_max, f1_avg), dim=1)
-        #
-        # f2_max = torch.nn.functional.max_pool2d(feat2, kernel_size=feat2.size()[2:])
-        # f2_avg = nn.functional.avg_pool2d(feat2, kernel_size=feat2.size()[2:])
-        # f2 = torch.cat((f2_max, f2_avg), dim=1)
+        f1_max = torch.nn.functional.max_pool2d(feat1, kernel_size=feat1.size()[2:])
+        f1_avg = nn.functional.avg_pool2d(feat1, kernel_size=feat1.size()[2:])
+        f1 = torch.cat((f1_max, f1_avg), dim=1)
+
+        f2_max = torch.nn.functional.max_pool2d(feat2, kernel_size=feat2.size()[2:])
+        f2_avg = nn.functional.avg_pool2d(feat2, kernel_size=feat2.size()[2:])
+        f2 = torch.cat((f2_max, f2_avg), dim=1)
 
         f3 = torch.sub(f1, f2)
         f3 = torch.mul(f3, f3)
@@ -105,8 +87,7 @@ class SiameseNetwork(nn.Module):
         f2_ = torch.mul(f2, f2)
         f4 = torch.sub(f1_, f2_)
         feat = torch.cat((f4, f3), dim=1)
-        # feat = feat.view(feat.size()[0], -1)
-        # feat = torch.cat((f1, f2), dim=1)
+        feat = feat.view(feat.size()[0], -1)
 
         output = self.classifier(feat)
         return output
@@ -120,17 +101,8 @@ class SiameseNetwork(nn.Module):
 
         # self.model.classifier[-2].apply(init_weights)
         self.classifier.apply(init_weights)
-
-        trainable_params = 0
-        non_trainable_params = 0
-        for p in self.parameters():
-            if p.requires_grad:
-                trainable_params += np.prod(p.shape)
-
-        cl_params = 17684919
-
-        trainable_model_parameters = filter(lambda p: p.requires_grad, self.features.parameters())
-        non_trainable_model_parameters = filter(lambda p: not (p.requires_grad), self.features.parameters())
+        trainable_model_parameters = filter(lambda p: p.requires_grad, self.parameters())
+        non_trainable_model_parameters = filter(lambda p: not (p.requires_grad), self.parameters())
         trainable_params = sum([np.prod(p.size()) for p in trainable_model_parameters])
         non_trainable_params = sum([np.prod(p.size()) for p in non_trainable_model_parameters])
         print("Num. of trainable parameters: {:,}, num. of frozen parameters: {:,}, total: {:,}".format(
--- code/main.py
+++ code/main.py
@@ -23,8 +23,8 @@ CREATE_SUBMISSION = False
 
 # Hyper params
 hyper_params = {
-    "init_lr": 1e-5,
-    "BATCH_SIZE": 32,
+    "init_lr": 1e-3,
+    "BATCH_SIZE": 16,
     "NUMBER_EPOCHS": 200,
     "weight_decay": 0,
     "decay_lr": True,
