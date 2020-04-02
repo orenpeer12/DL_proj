653ea33c13204cd404429e96d583b7bb53f825ff
Oren Peer <oren.peer2@gmail.com>
2020-04-02 21:50:29 +0300

    fixed my stupid code...

--- code/create_ensemble.py
+++ code/create_ensemble.py
@@ -25,7 +25,6 @@ device = torch.device('cuda: ' + str(GPU_ID) if torch.cuda.is_available() else '
 ensemble_name = 'en4'
 root_folder = Path(os.getcwd())
 submission_folder = root_folder / 'submissions_files' / ensemble_name
-BinaryEnsemble = False
 
 # get the sample submission file for loading pairs, and create the new submission file.
 sample_submission_path = root_folder / 'data' / 'faces' / 'sample_submission.csv'
@@ -37,79 +36,23 @@ submissions = [s for s in os.listdir(submission_folder) if s.endswith('.csv') an
 submissions_paths = [root_folder / 'submissions_files' / ensemble_name / s for s in submissions]
 print('Creating ensemble submission out of {} submissions!'.format(len(submissions)))
 
-if BinaryEnsemble:
-    ensemble_csvs = [pd.read_csv(s) for s in submissions_paths]
-    # for (lines) in zip(ensemble_csvs):
-    #     lines +=1
-    zero_one_count = np.zeros((ensemble_csvs[0].count(axis=0)[0],2))
+ensemble_csvs = [pd.read_csv(s) for s in submissions_paths]
+all_sigmoids = np.vstack([np.array(csv.is_related) for csv in ensemble_csvs])
 
-    for csv_file in ensemble_csvs:
-        for line_num, is_related in enumerate(csv_file.is_related):
-            zero_one_count[line_num][is_related] += 1
+res1 = np.mean(all_sigmoids, axis=0)
+# a = np.abs(res -.5)
+# b = []
+# for i in range(a.shape[1]):
+#     best_idx_in_line = np.argmax(a[:, i])
+#     b.append(res[best_idx_in_line][i])
+# res = np.array(b)
+df_submit.is_related = res1
 
-    res = np.argmax(zero_one_count, axis=1)
-    ties = np.where(zero_one_count[:, 0] == zero_one_count[:, 1])[0]
-    res[ties] = np.random.randint(0, 1, len(ties))
-
-    df_submit.is_related = res
-
-    # write submission
-    df_submit['is_related'].value_counts()
-    res = df_submit.to_csv(dst_submission_path, index=False)
-
-else:
-    mean = [91.4953, 103.8827, 131.0912]
-    std = [1, 1, 1]
-    test_transform = transforms.Compose([
-        transforms.ToTensor(),
-        scale_tensor_255,
-        rgb2bgr,
-        transforms.Normalize(mean=mean,
-                             std=std)
-    ])
-    # load test-set
-    testset = TestDataset(df=df_submit, root_dir=root_folder / 'data' / 'faces' / 'test', transform=test_transform)
-    test_loader = torch.utils.data.DataLoader(testset, batch_size=16)
-    # load models-paths and loacate the best validation-accuracy-models
-    models_names = [m.split('_')[0] for m in submissions]
-    best_models_names = [get_best_model(root_folder / 'models' / m) for m in models_names]
-    outputs = []
-    for mn, bmn in zip(models_names, best_models_names):
-        print("evaluating " + mn)
-        cur_model_evaluation = []
-        # load model
-        net = torch.load(root_folder / 'models' / mn / bmn.replace('.pt', '_model.pt'))
-        net.load_state_dict(torch.load(root_folder / 'models' / mn / bmn.replace('.pt', '_state.pt')))
-
-        # pass testset through model:
-        net.eval()
-        net.to(device)
-        for i, data in enumerate(test_loader, 0):
-            row, img0, img1 = data
-            row, img0, img1 = row.to(device), img0.to(device), img1.to(device)
-
-            output = net(img0, img1)
-            # predicted = torch.round(output.data).long().view(-1)
-            predicted = output.data.view(-1)
-            cur_model_evaluation.append(predicted.data.cpu().numpy())
-        outputs.append(np.concatenate(cur_model_evaluation))
-
-    # write submission
-    res = np.vstack(outputs)
-    res = np.mean(res, axis=0)
-    # a = np.abs(res -.5)
-    # b = []
-    # for i in range(a.shape[1]):
-    #     best_idx_in_line = np.argmax(a[:, i])
-    #     b.append(res[best_idx_in_line][i])
-    # res = np.array(b)
-    df_submit.is_related = res
-
-    # write submission
-    res1 = df_submit.to_csv(dst_submission_path, index=False)
-    # submit file
-    os.environ["KAGGLE_CONFIG_DIR"] = str(root_folder / '..')
-    os.system('kaggle competitions submit -c recognizing-faces-in-the-wild -f ' +
-              str(dst_submission_path) + ' -m ' + ensemble_name + '.csv')
-    # show submissions
-    os.system('kaggle competitions submissions recognizing-faces-in-the-wild')
+# write submission
+res1 = df_submit.to_csv(dst_submission_path, index=False)
+# submit file
+os.environ["KAGGLE_CONFIG_DIR"] = str(root_folder / '..')
+os.system('kaggle competitions submit -c recognizing-faces-in-the-wild -f ' +
+          str(dst_submission_path) + ' -m ' + ensemble_name + '.csv')
+# show submissions
+os.system('kaggle competitions submissions recognizing-faces-in-the-wild')
